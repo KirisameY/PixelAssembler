feac362be89c16ba058a1b89ce645bfea1255600
霧雨烨 <Web_13234@outlook.com>
2026-06-02 10:45:45 +0800

    第一步

---
+++ PixelAssembler/GUI/GraphMaps/BaseGraphMap.cs
@@ -0,0 +1,28 @@
+﻿using Godot;
+
+namespace PixelAssembler.GUI.GraphMaps;
+
+public abstract partial class BaseGraphMap : GraphEdit
+{
+    public sealed override void _Ready()
+    {
+        ConnectionRequest += (from, fromPort, to, toPort) =>
+        {
+            var fromNode = GetNode<GraphNode>(from.ToString());
+            var toNode = GetNode<GraphNode>(to.ToString());
+            if (OnConnectionRequest(fromNode, fromPort, toNode, toPort)) ConnectNode(from, (int)fromPort, to, (int)toPort);
+        };
+        DisconnectionRequest += (from, fromPort, to, toPort) =>
+        {
+            var fromNode = GetNode<GraphNode>(from.ToString());
+            var toNode = GetNode<GraphNode>(to.ToString());
+            if (OnDisconnectionRequest(fromNode, fromPort, toNode, toPort)) DisconnectNode(from, (int)fromPort, to, (int)toPort);
+        };
+        AfterReady();
+    }
+
+    protected virtual void AfterReady() { }
+
+    protected abstract bool OnConnectionRequest(GraphNode from, long fromPort, GraphNode to, long toPort);
+    protected abstract bool OnDisconnectionRequest(GraphNode from, long fromPort, GraphNode to, long toPort);
+}
---
+++ PixelAssembler/GUI/GraphMaps/MainGraphMap.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+using Godot;
+
+namespace PixelAssembler.GUI.GraphMaps;
+
+public partial class MainGraphMap : BaseGraphMap
+{
+    protected override bool OnConnectionRequest(GraphNode from, long fromPort, GraphNode to, long toPort)
+    {
+        return true;
+    }
+    protected override bool OnDisconnectionRequest(GraphNode from, long fromPort, GraphNode to, long toPort)
+    {
+        return true;
+    }
+}
