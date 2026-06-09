a6e8234595d2357de8ed0e7b8ef29e07320cdcf3
霧雨烨 <Web_13234@outlook.com>
2026-06-09 18:56:58 +0800

    新建节点菜单UI

---
+++ PixelAssembler/GUI/PopupMenus/AddGraphNodeMenu.cs
@@ -0,0 +1,36 @@
+﻿using Godot;
+
+namespace PixelAssembler.GUI.PopupMenus;
+
+public partial class AddGraphNodeMenu : PopupPanel
+{
+    #region Factory
+
+    private AddGraphNodeMenu() { }
+
+    private static PackedScene Scene => field ??= ResourceLoader.Load<PackedScene>("res://GUI/PopupMenus/AddGraphNodeMenu.tscn");
+
+    public static AddGraphNodeMenu CreateInstance()
+    {
+        return Scene.Instantiate<AddGraphNodeMenu>();
+    }
+
+    #endregion
+
+    #region SubNodes
+
+    private LineEdit SearchBox => field ??= GetNode<LineEdit>("VBoxContainer/SearchBox");
+
+    #endregion
+
+    public override void _Ready()
+    {
+        base._Ready();
+
+        // temp for testing
+        CreateTween().TweenCallback(Callable.From(() =>
+        {
+            PopupCentered();
+        }));
+    }
+}
